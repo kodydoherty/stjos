04b2c657d75b34b4b7d6f408f0427d1e233a50d8
Kody Doherty <kodydoherty@gmail.com>
2015-03-31 18:35:21 -0400

    first commit

---
+++ stjos/TuitionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  stjos
+//
+//  Created by Samuel Doherty on 3/17/15.
+//  Copyright (c) 2015 stjos. All rights reserved.
+//
+
+import UIKit
+
+class TuitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
